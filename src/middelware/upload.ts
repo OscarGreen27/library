ac6ae62b5eacff5b0d9d00f8a179f98e348fd62f
OscarGreen27 <andrii.voitenko97@gmail.com>
2026-06-14 16:45:22 +0300

    add temp file upload

--- src/middelware/upload.ts
+++ src/middelware/upload.ts
@@ -1,21 +1,60 @@
-import multer from "multer";
+import multer, { FileFilterCallback, MulterError } from "multer";
+import fsSync from "fs";
+import dotenv from "dotenv";
 import path from "path";
 
+dotenv.config();
+
 //middleware responsible for saving images
-const upload = multer({
+export const upload = multer({
+  /**
+   * Image is saved to a temporary folder for later movement.
+   * After checking the existence of the book id, the file will be moved to the appropriate folder
+   */
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      const imgPath = path.resolve(process.cwd(), "public", "images");
-      console.log(process.cwd);
-      console.log(imgPath);
-      cb(null, imgPath);
+      try {
+        const TEMP_COVER_PATH = process.env["TEMP_COVER_PATH"] || "/tmp/upload/lib-cover";
+
+        if (!fsSync.existsSync(TEMP_COVER_PATH)) {
+          fsSync.mkdirSync(TEMP_COVER_PATH, { recursive: true });
+        }
+        cb(null, TEMP_COVER_PATH);
+      } catch (err) {
+        if (err instanceof Error || err instanceof MulterError) {
+          cb(err, "");
+        }
+      }
     },
     filename: (req, file, cb) => {
-      const ext = path.extname(file.originalname);
-      const uniqueName = Math.round(Math.random() * 1e9) + ext;
-      cb(null, uniqueName);
+      try {
+        if (typeof req.query["id"] !== "string" || !/^\d+$/.test(req.query["id"])) {
+          throw new Error("ID is invalid");
+        }
+
+        const name = Date.now() + "-cover-" + req.query["id"] + "-" + Math.round(Math.random() * 1e9);
+
+        cb(null, name + path.extname(file.originalname));
+      } catch (err) {
+        if (err instanceof Error || err instanceof MulterError) {
+          cb(err, "");
+        }
+      }
     },
   }),
+  fileFilter: (req, file, cb) => {
+    const allowedMimeType = ["image/jpeg", "image/png", "image/webp"];
+    if (allowedMimeType.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error(`File type ${file.mimetype} not supported`));
+    }
+  },
+  limits: {
+    fileSize: 1024 * 1024 * Number(process.env["FILE_SIZE"]) || 5,
+    files: Number(process.env["FILES"]) || 1,
+    fields: Number(process.env["FIELDS"]) || 1,
+    parts: Number(process.env["PARTS"]) || 2,
+    fieldNameSize: Number(process.env["FIELD_NAME_SIZE"]) || 15,
+  },
 });
-
-export default upload;
