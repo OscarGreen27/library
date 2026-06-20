765ca65b559a57bbb207cf92d310af51c4c833f3
OscarGreen27 <andrii.voitenko97@gmail.com>
2026-06-20 21:12:45 +0300

    removed idValidator middleware from all routers

--- src/routers/author-router.ts
+++ src/routers/author-router.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import validateId from "../middelware/id-validator.js";
 import * as authorController from "../controllers/author-controller.js";
-import { authorize } from "../middelware/auth-check.js";
+import { authorize } from "../middleware/auth-check.js";
 
 const authorRouter = express.Router();
 
 authorRouter.get("/all", authorize("user"), authorController.getAuthors);
-authorRouter.get("/:id", authorize("user"), validateId, authorController.getAuthor);
+authorRouter.get("/:id", authorize("user"), authorController.getAuthor);
 authorRouter.post("/add", authorize("admin"), authorController.addAuthor);
-authorRouter.delete("/delete/:id", authorize("admin"), validateId, authorController.deleteAuthor);
+authorRouter.delete("/delete/:id", authorize("admin"), authorController.deleteAuthor);
 
 export default authorRouter;
--- src/routers/book-router.ts
+++ src/routers/book-router.ts
@@ -1,14 +1,13 @@
 import express from "express";
-import validateId from "../middelware/id-validator.js";
 import * as bookController from "../controllers/book-controller.js";
-import { authorize } from "../middelware/auth-check.js";
+import { authorize } from "../middleware/auth-check.js";
 
 const bookRouter = express.Router();
 
 bookRouter.get("/all", authorize("user"), bookController.getAllBooks);
-bookRouter.get("/:id", authorize("user"), validateId, bookController.getBook);
-bookRouter.post("/add", authorize("admin"), authorize, bookController.addBook);
-bookRouter.put("/want/:id", authorize("admin"), validateId, bookController.incrementWant);
-bookRouter.delete("/delete/:id", authorize("admin"), validateId, bookController.deleteBook);
+bookRouter.get("/:id", authorize("user"), bookController.getBook);
+bookRouter.post("/add", authorize("admin"), bookController.addBook);
+bookRouter.put("/want/:id", authorize("admin"), bookController.incrementWant);
+bookRouter.delete("/delete/:id", authorize("admin"), bookController.deleteBook);
 
 export default bookRouter;
